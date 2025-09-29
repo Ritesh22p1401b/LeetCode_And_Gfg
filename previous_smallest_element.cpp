33ee41f1acb36f26715fd75c63cbd0753f7b9e06
Ritesh Pandey <ritesh2897pandey@gmail.com>
2025-09-30 00:38:54 +0530

    pse

---
+++ previous_smallest_element.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> prev_ele(vector<int> arr){
+	vector<int> ans(arr.size(),0);
+	stack<int> s;
+
+	for(int i=0;i<arr.size();i++){
+
+		while(!s.empty() && s.top()>=arr[i]){
+		s.pop();
+		}
+		if(s.empty()){
+			ans[i]=-1;
+		}else{
+			ans[i]=s.top();
+		}
+		s.push(arr[i]);
+	}
+	return ans;
+}
+
+int main(){
+	int n;cin>>n;
+	vector<int> arr(n);
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	vector<int> ans = prev_ele(arr);
+	for(int val : ans){
+		cout<<val<<" ";
+	}
+	return 0;
+}
